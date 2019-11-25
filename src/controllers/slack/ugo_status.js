0286bb59d4e24e3ffa03d271b8ddb94fa8aee573
Tomás Altrui <tomas.altrui@widergy.com>
2019-11-24 22:55:15 -0300

    added ugo status

--- src/controllers/slack/ugo_status.js
+++ src/controllers/slack/ugo_status.js
@@ -4,12 +4,11 @@ import fetch from 'node-fetch';
 const router = express.Router();
 
 router.post('/', async function(req, res) {
-  const ugo_response = await fetch('https://utilitygo-api-dev.widergydev.com/health_check/all');
+  const ugo_res = await fetch('https://utilitygo-api-dev.widergydev.com/health_check/all');
 
-  console.log(ugo_response);
-  
+  const ugo_data = await ugo_res.json();  
 
-  res.send('Huesaaaa');
+  res.send(ugo_data.status === 200 ? 'La API de UGO está funcionando correctamente!' : 'La API de UGO se encuentra caida :(' );
 });
 
 export default router;
