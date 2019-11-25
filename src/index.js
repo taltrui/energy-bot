15583fa533d1ac65cecdfd10ea894c1a7529c019
Tomás Altrui <tomas.altrui@widergy.com>
2019-11-24 21:09:05 -0300

    slash command test

--- src/index.js
+++ src/index.js
@@ -9,27 +9,20 @@ import "regenerator-runtime/runtime";
 
 import express from 'express';
 import bodyParser from 'body-parser';
-import { createEventAdapter } from '@slack/events-api';
 import { postMessage } from './handlers/slack/PostMessage';
 
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const port = process.env.PORT || 3000
 const app = express();
-const slackEvents = createEventAdapter(slackSigningSecret);
-
-
-slackEvents.on('message', (event) => {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-  postMessage(`Qué onda ${event.user}, todo bien?`, event.channel)
-});
-
-app.use('/slack/events', slackEvents.requestListener());
 
 app.use(bodyParser());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+app.post('/hello', (req, res) => {
+  console.log(req);
+})
 
 app.listen(port, function() {
   console.log('Example app listening on port 3000!');
