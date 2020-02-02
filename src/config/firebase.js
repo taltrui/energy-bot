a5d10406fec1bcd881485511007e323b3b8e174d
Tomás Altrui <tomas.altrui@widergy.com>
2020-02-02 14:39:21 -0300

    updated babel to support es2020

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,7 @@ const serviceAccount = {
   type: 'service_account',
   project_id: process.env.FIRESTORE_PROYECT_ID,
   private_key_id: process.env.FIRESTORE_PRIV_KEY_ID,
-  private_key: process.env.FIRESTORE_PRIV_KEY.replace(/\\n/g, '\n'),
+  private_key: process.env.FIRESTORE_PRIV_KEY?.replace(/\\n/g, '\n'),
   client_email: process.env.FIRESTORE_CLIENT_EMAIL,
   client_id: process.env.FIRESTORE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
