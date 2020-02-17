b6ea9c9095a70dd9c257af9ccb5871be9ccb6acf
Tomás Altrui <tomas.altrui@widergy.com>
2020-02-17 18:42:13 -0300

    merge with dev

--- src/utils/slack.js
+++
@@ -1,6 +0,0 @@
-export const getSlackId = (employees, name) => {
-  const employee = employees.find(_employee => _employee.name === name) || {};
-  const { slack_id } = employee;
-
-  return slack_id ? `<@${slack_id}>` : name;
-};
